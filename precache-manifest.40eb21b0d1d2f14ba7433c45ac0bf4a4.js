8a7a4c7f51809d67eade5c5d341f4ffbdc5407fd
Jair Arteaga <jair_55_88@hotmail.com>
2020-01-08 23:24:58 -0500

    Updates

---
+++ precache-manifest.40eb21b0d1d2f14ba7433c45ac0bf4a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87fedef0a963a6d9b0d9fb081a62ef0",
+    "url": "/pokemonReact/index.html"
+  },
+  {
+    "revision": "12e15f538f894f3f9b3d",
+    "url": "/pokemonReact/static/css/main.cf3bb1be.chunk.css"
+  },
+  {
+    "revision": "2fca0ba1c3d15582f1db",
+    "url": "/pokemonReact/static/js/2.d86a13a6.chunk.js"
+  },
+  {
+    "revision": "12e15f538f894f3f9b3d",
+    "url": "/pokemonReact/static/js/main.fd1de2bd.chunk.js"
+  },
+  {
+    "revision": "262fe3dc93ba53b66cfc",
+    "url": "/pokemonReact/static/js/runtime-main.5d5ff479.js"
+  },
+  {
+    "revision": "b6bef9512a7c465aae75f75eb92b69cb",
+    "url": "/pokemonReact/static/media/background.b6bef951.jpg"
+  },
+  {
+    "revision": "aa75bdd5327132fc407902a92be5e262",
+    "url": "/pokemonReact/static/media/pokebola.aa75bdd5.png"
+  }
+]);
